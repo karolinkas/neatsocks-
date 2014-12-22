b8740bee633247498a5e18057bf63d5e9ea5f71c
Karolinka <li-nee@gmx.de>
2014-12-22 23:21:21 +0100

    added table

---
+++ app/assets/javascripts/designhelper.js
@@ -0,0 +1,15 @@
+var ready;
+
+
+ready = function() {
+
+	$('.pull-down').each(function() {
+	    $(this).css('margin-top', $(this).parent().height()-$(this).height())
+	});
+		
+	} 
+
+         
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
