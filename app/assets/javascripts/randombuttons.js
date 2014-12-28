99abfea844289a89506195c4c4432b27a8f8e884
Karolinka <li-nee@gmx.de>
2014-12-28 01:34:22 +0100

    get feature buttons from array randomly and without repetition

--- app/assets/javascripts/randombuttons.js
+++ app/assets/javascripts/randombuttons.js
@@ -1,16 +1,39 @@
 $(function() {
 
-	var featureNames = ["Buscador inteligente","Control del directo","Grabación","Aplicaciones","Multipantalla","Emisión","Sugerencias" ]
+    var featureNames = ["Buscador inteligente", "Control del directo", "Grabación", "Aplicaciones", "Multipantalla", "Emisión", "Sugerencias"]
 
-	for (i=0; i<7; i++) { 
 
-		var randomIndices = Math.floor(Math.random() * (i+1));
-		
-		
-	}
-		$(".feature").html(featureNames[randomIndices]); 
+    // var randomIndex = Math.floor(Math.random()  * (7 - 1) + 1)
 
-	
+    function shuffle(array) {
+        var i = array.length,
+            j = 0,
+            temp;
+
+        while (i--) {
+
+            j = Math.floor(Math.random() * (i + 1));
+
+            // swap randomly chosen element with current element
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+
+        }
+
+        return array;
+    }
+
+    var randNames = shuffle(featureNames);
+    console.log(randNames);
+
+
+
+    $(".feature:nth-child(1)").html(randNames[0]);
+    $(".feature:nth-child(2)").html(randNames[1]);
+    $(".feature:nth-child(3)").html(randNames[2]);
+
+
 
 
 
