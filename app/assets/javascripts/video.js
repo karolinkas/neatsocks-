75c9d83d15ea527cdef590d18a3afc209eafb99c
Karolinka <li-nee@gmx.de>
2015-01-01 17:41:31 +0100

    play/pause/rewind video

--- app/assets/javascripts/video.js
+++ app/assets/javascripts/video.js
@@ -1,14 +1,26 @@
 function Video(){
 
+	
+
 	this.play = function(){
+		
 		document.getElementById('introvideo').play();	
+		console.log(currentTime);
 	}
+
 	this.pause = function(){
 		document.getElementById('introvideo').pause();	
 	}
+
+	this.rewind = function(){
+		document.getElementById('introvideo').pause();
+		document.getElementById('introvideo').currentTime = 0;	
+	}
+
 	this.volUp = function(){
 		document.getElementById('introvideo').volume+=0.1;	
 	}
+
 	this.volDown = function(){
 		document.getElementById('introvideo').volume-=0.1;	
 	}
